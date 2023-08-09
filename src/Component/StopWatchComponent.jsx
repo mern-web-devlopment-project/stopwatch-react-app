3beea6a6413155f92e8b262f9136ccc1e19e58dc
nagaraj6618 <nagaraj516700@gmail.com>
2023-08-09 21:24:15 +0530

    Start Funtion added

--- src/Component/StopWatchComponent.jsx
+++ src/Component/StopWatchComponent.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './StopWatchComponent.css'
 const StopWatchComponent = () => {
+ 
+  const [minute,setMinute] = useState("00")
+  const [second,setSecond] = useState("00")
+  const [milliSecond,setMilliSecond] = useState("00")
+  const [state,setState] = useState(false);
+  const resetHandler = () =>{
+    setMilliSecond("00");
+    setSecond("00");
+    setMinute("00");
+  }
+  const startHandler = () => {
+    var seconds = 0
+    var count =0;
+    var minutes = 0
+     setInterval(() => {
+      if(count===99){
+        count =0;
+        seconds+=1;
+      }
+      if(seconds ===60){
+        minutes+=1;
+        seconds =0;
+      }
+      count +=1;
+      setSecond(seconds)
+      setMilliSecond(count)
+      setMinute(minutes)
+    }, 1);
+  } 
   return (
     <div className='header'>
       <div className="wrapper">
         <div className="timer">
-          <div className='stopwatch'>12 : 23 : 89</div>
+          <div className='stopwatch'>{minute} : {second} :  {milliSecond}</div>
         </div>
         <div className="button">
-          <button className='btn btn1'>Reset</button>
-          <button className='btn btn2'>Play</button>
+          <button className='btn btn1' onClick={resetHandler}>Reset</button>
+          <button className='btn btn2' onClick={startHandler}>Play</button>
           <button className='btn btn3'>Pause</button>
         </div>
       </div>
